b9d4ae0dc5504fdca9f2ce31d97ad86edfd1346c
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-12-14 20:05:35 +0200

    test solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,99 @@
 'use strict';
+
+const div = document.createElement('div');
+const body = document.body;
+let leftClicked = false;
+let rightClicked = false;
+
+div.setAttribute('data-qa', 'notificatio');
+
+function promOne(params) {
+  const firstPromise = new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject(new Error('First promise was rejected'));
+    }, 3000);
+
+    document.addEventListener('click', () => {
+      clearTimeout(timeoutId);
+
+      resolve('First promise was resolved');
+    });
+  });
+
+  return firstPromise;
+}
+
+async function promFirst() {
+  try {
+    const dataOne = await promOne();
+
+    div.textContent = dataOne;
+    div.classList.add('success');
+
+    body.append(div);
+  } catch (error) {
+    div.textContent = error.message;
+    div.classList.add('error');
+
+    body.append(div);
+  }
+}
+promFirst();
+
+function promTwo(params) {
+  const secondPromise = new Promise((resolve) => {
+    body.addEventListener('click', () => {
+      resolve('Second promise was resolved');
+    });
+
+    body.addEventListener('contextmenu', () => {
+      resolve('Second promise was resolved');
+    });
+  });
+
+  return secondPromise;
+}
+
+async function promSecond() {
+  const dataTwo = await promTwo();
+
+  div.textContent = dataTwo;
+  div.classList.add('success');
+
+  body.append(div);
+}
+promSecond();
+
+function promThree(params) {
+  const thirdPromise = new Promise((resolve, reject) => {
+    body.addEventListener('click', () => {
+      leftClicked = true;
+    });
+
+    body.addEventListener('contextmenu', (ev) => {
+      ev.preventDefault();
+
+      rightClicked = true;
+    });
+
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  return thirdPromise;
+}
+
+async function promThird() {
+  try {
+    const dataThree = await promThree();
+
+    div.textContent = dataThree;
+    div.classList.add('success');
+
+    body.append(div);
+  } catch (error) {
+    div.classList.add('error');
+  }
+}
+promThird();
